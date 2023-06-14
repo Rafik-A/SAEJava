4a96745f51e37e52c5b1b49104a2a935417ecf24
Rafik <rafik.azzi@etu.univ-lyon1.fr>
2023-06-14 13:57:40 +0200

    arraylist

--- Application/graphe/Graphe.java
+++ Application/graphe/Graphe.java
@@ -2,7 +2,13 @@ package graphe;
 
 import vue.*;
 import java.util.ArrayList;
+import java.io.file;
 
 public class Graphe {
-    
+    private ArrayList<String>();
+    private ArrayList<ArrayList<String>>();
+
+    public void chargerGraphe(File file) {
+
+    }
 }
