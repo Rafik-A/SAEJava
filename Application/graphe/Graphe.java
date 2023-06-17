303700202a0d76069ca6a7063cb966b2d9c6d16c
Rafik <rafik.azzi@etu.univ-lyon1.fr>
2023-06-17 14:41:26 +0200

    maj

--- Application/graphe/Graphe.java
+++ Application/graphe/Graphe.java
@@ -8,6 +8,16 @@ public class Graphe {
     private ArrayList<Arete>() listeAretes;
     private ArrayList<Sommet>() listeSommets;
 
+    public Graphe() {
+        ArrayList<Arete>() listeAretes = new ArrayList<Arete>();
+        ArrayList<Sommet>() listeSommets = new ArrayList<Sommet>();
+    }
+
+    public void ajouterSommet(String nom, TypeSommet type) {
+        Sommet s = new Sommet(nom, type);
+        listeSommets.add(s);
+    }
+
     public void chargerGraphe(File file) {
 
     }
